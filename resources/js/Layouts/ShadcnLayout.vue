0cd27488014570536e1b5b744a1dcb28e0931803
amdlemos <amdlemos@gmail.com>
2025-02-05 14:12:38 -0300

    feat: create Layout with shadcn-vue components
    Implemented a base Layout using shadcn-vue components to structure the application.
    This will serve as the foundation for building new pages.

--- resources/js/Layouts/ShadcnLayout.vue
+++ resources/js/Layouts/ShadcnLayout.vue
@@ -507,14 +507,15 @@ function setActiveTeam(team: (typeof data.teams)[number]) {
                 </div>
             </header>
             <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-                <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div class="aspect-video rounded-xl bg-muted/50" />
-                    <div class="aspect-video rounded-xl bg-muted/50" />
-                    <div class="aspect-video rounded-xl bg-muted/50" />
-                </div>
-                <div
-                    class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"
-                />
+                <!-- <div class="grid auto-rows-min gap-4 md:grid-cols-3"> -->
+                <!--     <div class="aspect-video rounded-xl bg-muted/50" /> -->
+                <!--     <div class="aspect-video rounded-xl bg-muted/50" /> -->
+                <!--     <div class="aspect-video rounded-xl bg-muted/50" /> -->
+                <!-- </div> -->
+                <!-- <div -->
+                <!--     class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" -->
+                <!-- /> -->
+                <slot />
             </div>
         </SidebarInset>
     </SidebarProvider>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,15 @@ Route::get('/shadcn', function () {
     ]);
 });
 
+Route::get('/test', function () {
+    return Inertia::render('Test', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
